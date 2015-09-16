d06ebab059de4f4ea7ec2bb3ab43cf85d2931ca8
dev4s <ttofel@gmail.com>
2015-09-17 00:50:44 +0200

    Initial commit

---
+++ InstallProgramForJetBrainsPowerToy/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace InstallProgramForJetBrainsPowerToy
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.InstallAllVs2008 = new System.Windows.Forms.Button();
+            this.InstallAllVs2010 = new System.Windows.Forms.Button();
+            this.UninstallAllVs2010 = new System.Windows.Forms.Button();
+            this.UninstallAllVs2008 = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Enabled = false;
+            this.PathTextBox.Location = new System.Drawing.Point(12, 42);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(323, 20);
+            this.PathTextBox.TabIndex = 5;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(336, 40);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(81, 23);
+            this.BrowseButton.TabIndex = 0;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // InstallAllVs2008
+            // 
+            this.InstallAllVs2008.Enabled = false;
+            this.InstallAllVs2008.Location = new System.Drawing.Point(12, 68);
+            this.InstallAllVs2008.Name = "InstallAllVs2008";
+            this.InstallAllVs2008.Size = new System.Drawing.Size(405, 23);
+            this.InstallAllVs2008.TabIndex = 1;
+            this.InstallAllVs2008.Text = "Install All Programs For VS 2008";
+            this.InstallAllVs2008.UseVisualStyleBackColor = true;
+            this.InstallAllVs2008.Click += new System.EventHandler(this.InstallAllVs2008_Click);
+            // 
+            // InstallAllVs2010
+            // 
+            this.InstallAllVs2010.Enabled = false;
+            this.InstallAllVs2010.Location = new System.Drawing.Point(12, 97);
+            this.InstallAllVs2010.Name = "InstallAllVs2010";
+            this.InstallAllVs2010.Size = new System.Drawing.Size(405, 23);
+            this.InstallAllVs2010.TabIndex = 2;
+            this.InstallAllVs2010.Text = "Install All Programs For VS 2010";
+            this.InstallAllVs2010.UseVisualStyleBackColor = true;
+            this.InstallAllVs2010.Click += new System.EventHandler(this.InstallAllVs2010_Click);
+            // 
+            // UninstallAllVs2010
+            // 
+            this.UninstallAllVs2010.Enabled = false;
+            this.UninstallAllVs2010.Location = new System.Drawing.Point(12, 155);
+            this.UninstallAllVs2010.Name = "UninstallAllVs2010";
+            this.UninstallAllVs2010.Size = new System.Drawing.Size(405, 23);
+            this.UninstallAllVs2010.TabIndex = 4;
+            this.UninstallAllVs2010.Text = "Uninstall All Programs For VS 2010";
+            this.UninstallAllVs2010.UseVisualStyleBackColor = true;
+            this.UninstallAllVs2010.Click += new System.EventHandler(this.UninstallAllVs2010_Click);
+            // 
+            // UninstallAllVs2008
+            // 
+            this.UninstallAllVs2008.Enabled = false;
+            this.UninstallAllVs2008.Location = new System.Drawing.Point(12, 126);
+            this.UninstallAllVs2008.Name = "UninstallAllVs2008";
+            this.UninstallAllVs2008.Size = new System.Drawing.Size(405, 23);
+            this.UninstallAllVs2008.TabIndex = 3;
+            this.UninstallAllVs2008.Text = "Uninstall All Programs For VS 2008";
+            this.UninstallAllVs2008.UseVisualStyleBackColor = true;
+            this.UninstallAllVs2008.Click += new System.EventHandler(this.UninstallAllVs2008_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 12);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(405, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 187);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.UninstallAllVs2010);
+            this.Controls.Add(this.UninstallAllVs2008);
+            this.Controls.Add(this.InstallAllVs2010);
+            this.Controls.Add(this.InstallAllVs2008);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.PathTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Install PowerToys From JetBrains";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button InstallAllVs2008;
+        private System.Windows.Forms.Button InstallAllVs2010;
+        private System.Windows.Forms.Button UninstallAllVs2010;
+        private System.Windows.Forms.Button UninstallAllVs2008;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ InstallProgramForJetBrainsPowerToy/MainForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace InstallProgramForJetBrainsPowerToy
+{
+    public partial class MainForm : Form
+    {
+        private const string Msiexecinstall = "msiexec /i";
+        private const string Msiexecuninstall = "msiexec /x";
+        private const string Quiet = "/qn";
+        private const string Passive = "/passive";
+        private const string Vs2009 = "(VS%209.0)";
+        private const string Vs2010 = "(VS%2010.0)";
+        private const string CmdL = "cmd";
+        private const string CmdLProps = "/c start /wait";
+        private const string Resharper = "Resharper";
+
+        private void InstallOrUninstall(string msiexec, string vs)
+        {
+            var directoryInfo = new DirectoryInfo(PathTextBox.Text);
+            var files = directoryInfo.GetFiles();
+
+            var files2 =
+                files.Where(f => f.Extension == ".msi" && f.Name.EndsWith(String.Format("{0}{1}", vs, f.Extension))).ToList();
+            var temp = files2[files2.Count - 1];
+            files2[files2.Count - 1] = files2[files2.Count - 2];
+            files2[files2.Count - 2] = temp;
+
+            progressBar.Value = 0;
+            progressBar.Maximum = files2.Count;
+
+            foreach (var process in
+                files2.Select(f => GetProcessInfo(f.Name.StartsWith(Resharper) ? Passive : Quiet, msiexec, f)).Select(startInfo => Process.Start(startInfo)).Where(process => process != null))
+            {
+                process.WaitForExit();
+                progressBar.Increment(1);
+            }
+        }
+
+        private static ProcessStartInfo GetProcessInfo(string option, string msiexec, FileSystemInfo f)
+        {
+            return new ProcessStartInfo(CmdL, string.Format("{0} {1} {2} {3}", CmdLProps, msiexec, f.FullName, option)) { WindowStyle = ProcessWindowStyle.Hidden };
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK) return;
+            PathTextBox.Text = folderBrowserDialog.SelectedPath;
+
+            InstallAllVs2008.Enabled = true;
+            InstallAllVs2010.Enabled = true;
+            UninstallAllVs2008.Enabled = true;
+            UninstallAllVs2010.Enabled = true;
+        }
+
+        private void InstallAllVs2008_Click(object sender, EventArgs e)
+        {
+            InstallOrUninstall(Msiexecinstall, Vs2009);
+            MessageBox.Show("Installation Completed!", "Installation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void InstallAllVs2010_Click(object sender, EventArgs e)
+        {
+            InstallOrUninstall(Msiexecinstall, Vs2010);
+            MessageBox.Show("Installation Completed!", "Installation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void UninstallAllVs2008_Click(object sender, EventArgs e)
+        {
+            InstallOrUninstall(Msiexecuninstall, Vs2009);
+            MessageBox.Show("Uninstallation Completed!", "Uninstallation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void UninstallAllVs2010_Click(object sender, EventArgs e)
+        {
+            InstallOrUninstall(Msiexecuninstall, Vs2010);
+            MessageBox.Show("Uninstallation Completed!", "Uninstallation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
